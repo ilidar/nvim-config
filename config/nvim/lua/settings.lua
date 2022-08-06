30f10f5fb2c1bd596a8b6683c6dc17b228234412
Denys Kotelovych <denys.kotelovych@gmail.com>
2022-08-06 21:12:44 +0300

    Nvim misc configs;

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -61,3 +61,11 @@ opt.swapfile = false
 ------------------------------------------------------------
 opt.updatetime = 100
 opt.signcolumn = "yes"
+
+------------------------------------------------------------
+-- Misc
+------------------------------------------------------------
+g.loaded_python3_provider = 0
+g.loaded_node_provider = 0
+g.loaded_perl_provider = 0
+g.loaded_ruby_provider = 0
