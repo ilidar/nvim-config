b3c5c3075babf3bc98601f10e04549d71c80c4f4
Denys Kotelovych <denys.kotelovych@gmail.com>
2022-10-27 12:06:52 +0300

    highlighting;

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,8 @@
 require("nvim-treesitter.configs").setup({
     ensure_installed = { "python", "cpp", "lua", "vim" },
-    highlight = { enable = true },
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
     indent = { enable = true },
 })
