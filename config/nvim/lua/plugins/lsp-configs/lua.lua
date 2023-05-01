552a5cabc1670a373c878daff093e12bfbc8dd13
Denys Kotelovych <denys.kotelovych@gmail.com>
2023-05-01 20:33:22 +0300

    Config update;

--- config/nvim/lua/plugins/lsp-configs/lua.lua
+++ config/nvim/lua/plugins/lsp-configs/lua.lua
@@ -1,40 +1,12 @@
 local base = require("plugins.lsp-configs.base")
 
-local get_sumneko_system_name = function()
-    if vim.fn.has("mac") == 1 then
-        return "macOS"
-    elseif vim.fn.has("unix") == 1 then
-        return "Linux"
-    elseif vim.fn.has("win32") == 1 then
-        return "Windows"
-    else
-        return -1
-    end
-end
-
-local system_name = get_sumneko_system_name()
-if system_name == -1 then
-    print("Unsupported system name")
-    os.exit(1)
-end
-
-local sumneko_root_path = os.getenv("HOME") .. "/.deps/lua-language-server"
-local sumneko_binary = sumneko_root_path .. "/bin/lua-language-server"
-
-local runtime_path = vim.split(package.path, ";")
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
-require("lspconfig").sumneko_lua.setup({
+require("lspconfig").lua_ls.setup({
     on_attach = base.lsp_on_attach,
-    cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
     settings = {
         Lua = {
             runtime = {
                 -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
                 version = "LuaJIT",
-                -- Setup your lua path
-                path = runtime_path,
             },
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
