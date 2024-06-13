bafa547e919c4fb16d7d9311718f724f56961e7d
Denys Kotelovych <denys.kotelovych@gmail.com>
2024-06-13 14:05:20 +0300

    Treesitter;

--- config/nvim/lua/plugin_cfg/treesitter.lua
+++ config/nvim/lua/plugin_cfg/treesitter.lua
@@ -1,6 +1,29 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-        ensure_installed = { "python", "cpp", "lua", "vim", "markdown" },
-    },
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            highlight = {
+                enable = true,
+            },
+            indent = { enable = true },
+            ensure_installed = {
+                "json",
+                "yaml",
+                "html",
+                "markdown",
+                "markdown_inline",
+                "bash",
+                "lua",
+                "vim",
+                "dockerfile",
+                "gitignore",
+                "vimdoc",
+                "c",
+                "cpp",
+                "python",
+            },
+        })
+    end,
 }
