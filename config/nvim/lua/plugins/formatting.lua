21ff5d4701faa13918614f413211cf75c3fd5a38
Denys Kotelovych <denys.kotelovych@gmail.com>
2026-01-24 15:28:04 +0200

    disable format on save;

--- config/nvim/lua/plugins/formatting.lua
+++ config/nvim/lua/plugins/formatting.lua
@@ -29,9 +29,6 @@ return {
             cpp = { "clang-format" },
             rust = { "rustfmt" },
         },
-        format_on_save = {
-            timeout_ms = 500,
-            lsp_format = "fallback",
-        },
+        format_on_save = false,
     },
 }
