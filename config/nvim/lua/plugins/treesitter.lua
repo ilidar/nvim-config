0750a326ee0331d235706f8ca9df6e9a52cf0adf
Denys Kotelovych <denys.kotelovych@gmail.com>
2026-02-03 20:09:57 +0200

    revert treesitter to master branch;

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,33 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    branch = "main",
+    branch = "master",
     build = ":TSUpdate",
     lazy = false,
+    opts = {
+        ensure_installed = {
+            "json",
+            "yaml",
+            "html",
+            "markdown",
+            "markdown_inline",
+            "bash",
+            "lua",
+            "vim",
+            "vimdoc",
+            "dockerfile",
+            "gitignore",
+            "c",
+            "cpp",
+            "python",
+            "rust",
+            "typescript",
+            "javascript",
+            "css",
+        },
+        highlight = { enable = true },
+        indent = { enable = true },
+    },
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+    end,
 }
